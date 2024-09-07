51bdd53e16ab53cfd039c26e9d1a493d8e557b8c
americofp <americofp@yahoo.com.br>
2024-09-07 15:28:44 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -31,19 +31,27 @@ function pesquisar() {
                 <h2>
                     <a href="#" target="_blank">Doença: ${dado.nome}</a>
                 </h2>
-                <p class="descricao-meta"><h3>Outros nomes:</h3> ${dado.outros}</p>
+                
                 <p class="descricao-meta"><h3>Agente(s) etiológico(s):</h3> ${dado.agentes}</p>
                 <p class="descricao-meta"><h3>Tipo de agente:</h3> ${dado.tipoAgente}</p>
                 <p class="descricao-meta"><h3>Sobre a doença:</h3> ${dado.descricao}</p>
+                <p class="descricao-meta"><h3>Principais sinais e sintomas:</h3> ${dado.sintomas}</p>
                 <p class="descricao-meta"><h3>Incubação:</h3> ${dado.periodoIncubacao}</p>
+
+                <p class="descricao-meta"><h3>Sazonalidade:</h3> ${dado.szonalidade}</p>
+                <p class="descricao-meta"><h3>Distribuição Geográfica:</h3> ${dado.distribuicaoGeografica}</p>
+                <p class="descricao-meta"><h3>Principais grupos de risco:</h3> ${dado.gruposRisco}</p>
                 <p class="descricao-meta"><h3>Transmissão:</h3> ${dado.transmissao}</p>
+                
                 <p class="descricao-meta"><h3>Hospedeiro(s) definitivo(s):</h3> ${dado.definitivo}</p>
                 <p class="descricao-meta"><h3>Hospedeiro(s) Intermediário(s):</h3> ${dado.intermediario}</p>
                 <p class="descricao-meta"><h3>Reservatório(s):</h3> ${dado.reservatorio}</p>
-                <p class="descricao-meta"><h3>Sinais e sintomas:</h3> ${dado.sintomas}</p>
-                <p class="descricao-meta"><h3>Tratamento:</h3> ${dado.tratamento}</p>
-                <p class="descricao-meta"><h3>Prevenção:</h3> ${dado.prevencao}</p>
                 
+                <p class="descricao-meta"><h3>Tratamento:</h3> ${dado.tratamento}</p>
+                <p class="resistencia-meta"><h3>Resistência a medicamentos:</h3> ${dado.resistencia}</p>
+                <p class="descricao-meta"><h3>Prevenção:</h3> ${dado.prevencao}</p>
+                <p class="descricao-meta"><h3>Outros nomes:</h3> ${dado.outros}</p>
+
 <p><h3>Mais Informações acesse:</h3>
 <ul style="list-style-type: circle; list-style-position: outside;">
     <li><p><a href="https://bvsms.saude.gov.br/" target="_blank">Biblioteca Virtual em Saúde (BVS)</a></li></p>
