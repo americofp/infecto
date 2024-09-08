b598a51107e79658589094101aae8a0dc5e9ee67
americofp <americofp@yahoo.com.br>
2024-09-08 18:25:53 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -66,3 +66,55 @@ function pesquisar() {
     // Atribui os resultados gerados à seção HTML
     section.innerHTML = resultados;
 }
+
+
+
+
+function mostrarAleatorio() {
+    // Obtém a lista de dados
+    let dados = getDados();
+  
+    // Gera um índice aleatório
+    let indiceAleatorio = Math.floor(Math.random() * dados.length);
+  
+    // Seleciona doença aleatória
+    let doencaSorte = dados[indiceAleatorio];
+  
+    // Exibe a doença na seção de resultados
+    let section = document.getElementById("resultados-pesquisa");
+    section.innerHTML = `
+
+      <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">Doença: ${doencaSorte.nome}, ${doencaSorte.outros}</a>
+                </h2>
+                
+                <p class="descricao-meta"><h3>Agente(s) etiológico(s):</h3> ${doencaSorte.agentes} - (${doencaSorte.tipoAgente})</p>
+                <p class="descricao-meta"><h3>Sobre a doença:</h3> ${doencaSorte.descricao}</p>
+                <p class="descricao-meta"><h3>Principais sinais e sintomas:</h3> ${doencaSorte.sintomas}</p>
+                
+                <p class="descricao-meta"><h3>Sazonalidade e distribuição geográfica:</h3> ${doencaSorte.sazonalidade} - ${doencaSorte.distribuicaoGeografica}.</p>
+                <p class="descricao-meta"><h3>Principais grupos de risco:</h3> ${doencaSorte.gruposRisco}</p>
+                <p class="descricao-meta"><h3>Transmissão:</h3> ${doencaSorte.transmissao}. Tempo de incubação: ${doencaSorte.periodoIncubacao}</p>
+                
+                <p class="descricao-meta"><h3>Hospedeiros:</h3> Hospedeiros definitivos: ${doencaSorte.definitivo} / Hospedeiros intermediários: </h3> ${doencaSorte.intermediario}</p>
+                <p class="descricao-meta"><h3>Reservatórios: </h3> ${doencaSorte.reservatorio}</p>
+                
+                <p class="descricao-meta"><h3>Tratamento:</h3> ${doencaSorte.tratamento}. Resistência ao tratamento: ${doencaSorte.resistencia}.</p>
+                <p class="descricao-meta"><h3>Prevenção:</h3> ${doencaSorte.prevencao}</p>
+
+<p><h3>Mais Informações acesse:</h3>
+<ul style="list-style-type: circle; list-style-position: outside;">
+    <li><p><a href="https://bvsms.saude.gov.br/" target="_blank">Biblioteca Virtual em Saúde (BVS)</a></li></p>
+    <li><p><a href="https://www.cdc.gov/" target="_blank">Centro de Prevenção e Controle de Doenças americano (CDC)</a></li></p>
+    <li><p><a href="https://www.who.int/" target="_blank">Organização Mundial da Saúde (WHO)</a></li></p>
+    <li><p><a href="https://www.paho.org/pt/brasil" target="_blank">Organização Panamericana da Saúde (OPAS)</a></li></p>
+    
+</ul>    
+            </div>
+    `;
+  }
+  
+  function getDados() {
+    return dados;
+  }
