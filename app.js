18c2b2d5129fe5540fea7db3cd2eeea8a32d2752
americofp <americofp@yahoo.com.br>
2024-09-08 01:25:12 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -32,23 +32,18 @@ function pesquisar() {
                     <a href="#" target="_blank">Doença: ${dado.nome}, ${dado.outros}</a>
                 </h2>
                 
-                <p class="descricao-meta"><h3>Agente(s) etiológico(s):</h3> ${dado.agentes}</p>
-                <p class="descricao-meta"><h3>Tipo de agente:</h3> ${dado.tipoAgente}</p>
+                <p class="descricao-meta"><h3>Agente(s) etiológico(s):</h3> ${dado.agentes} - (${dado.tipoAgente})</p>
                 <p class="descricao-meta"><h3>Sobre a doença:</h3> ${dado.descricao}</p>
                 <p class="descricao-meta"><h3>Principais sinais e sintomas:</h3> ${dado.sintomas}</p>
-                <p class="descricao-meta"><h3>Incubação:</h3> ${dado.periodoIncubacao}</p>
-
-                <p class="descricao-meta"><h3>Sazonalidade:</h3> ${dado.sazonalidade}</p>
-                <p class="descricao-meta"><h3>Distribuição Geográfica:</h3> ${dado.distribuicaoGeografica}</p>
+                
+                <p class="descricao-meta"><h3>Sazonalidade e distribuição geográfica:</h3> ${dado.sazonalidade} - ${dado.distribuicaoGeografica}.</p>
                 <p class="descricao-meta"><h3>Principais grupos de risco:</h3> ${dado.gruposRisco}</p>
-                <p class="descricao-meta"><h3>Transmissão:</h3> ${dado.transmissao}</p>
+                <p class="descricao-meta"><h3>Transmissão:</h3> ${dado.transmissao}. Tempo de incubação: ${dado.periodoIncubacao}</p>
                 
-                <p class="descricao-meta"><h3>Hospedeiro(s) definitivo(s):</h3> ${dado.definitivo}</p>
-                <p class="descricao-meta"><h3>Hospedeiro(s) Intermediário(s):</h3> ${dado.intermediario}</p>
-                <p class="descricao-meta"><h3>Reservatório(s):</h3> ${dado.reservatorio}</p>
+                <p class="descricao-meta"><h3>Hospedeiros:</h3> Hospedeiros definitivos: ${dado.definitivo} / Hospedeiros intermediários: </h3> ${dado.intermediario}</p>
+                <p class="descricao-meta"><h3>Reservatórios: </h3> ${dado.reservatorio}</p>
                 
-                <p class="descricao-meta"><h3>Tratamento:</h3> ${dado.tratamento}</p>
-                <p class="resistencia-meta"><h3>Resistência a medicamentos:</h3> ${dado.resistencia}</p>
+                <p class="descricao-meta"><h3>Tratamento:</h3> ${dado.tratamento}. Resistência ao tratamento: ${dado.resistencia}.</p>
                 <p class="descricao-meta"><h3>Prevenção:</h3> ${dado.prevencao}</p>
 
 <p><h3>Mais Informações acesse:</h3>
